be2518bb908bb263f42ed06993b3066a9ccb8263
Iago Suzart Silva <151882235+iaguinho-0@users.noreply.github.com>
2024-11-05 09:28:49 -0300

    Update Footer and logo.

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -7,10 +7,10 @@ export const HomeBanner: React.FC = () => {
     <div className="flex flex-col md:flex-row items-center justify-center min-h-[calc(100vh-4rem)] w-full bg-white text-black p-6 mt-14 md:p-12">
       {/* Texto à esquerda */}
       <div className="md:w-1/2 space-y-4 text-center md:text-left pl-4">
-        <h1 className="text-4xl md:text-7xl font-bold">
+        <h1 className="text-4xl md:text-7xl font-bold"  style={{ fontSize: 'clamp(26px, 3.5vw, 4.5rem)' }}>
           Soluções Inteligentes em Manutenção de Estoque e Entrega de Produtos
         </h1>
-        <h2 className="text-xl md:text-4xl text-gray-700">
+        <h2 className="text-xl md:text-4xl text-gray-700"  style={{ fontSize: 'clamp(14px, 2.75vw, 2.25rem)' }}>
           A mais nova e disruptiva forma de otimizar estoque e entrega.
         </h2>
       </div>
